bcd4c41ea5bad05172804f737eab87626c5a9dcf
Dsharma724 <dhairaysharma7@gmail.com>
2026-03-23 15:03:03 +0530

    Initial commit - Use Case 1 Book My Stay App

---
+++ BookMyStay.java
@@ -0,0 +1,18 @@
+
+public class BookMyStay {
+
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("===================================");
+        System.out.println("     Welcome to Book My Stay App   ");
+        System.out.println("===================================");
+
+        // Application details
+        System.out.println("Application Name : Book My Stay");
+        System.out.println("Version          : v1.0");
+
+        // Status message
+        System.out.println("\nApplication Started Successfully!");
+    }
+}
